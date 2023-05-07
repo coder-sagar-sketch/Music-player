5f26b8998c7a301e3efbc93edb0fd42189ac9df7
Shubham Sagar <86291350+coder-sagar-sketch@users.noreply.github.com>
2023-05-08 00:46:42 +0530

    create homepage

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Sidebar() {
+  return (
+    <Container>
+      
+        </Container>
+  )
+}
+
+const Container = styled.div`
+background-color: black;
+`;
